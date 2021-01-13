dd84d069e987999db5d784081c2a9701fcec1640
Filipan <filipandani@gamil.com>
2021-01-13 19:38:28 +0200

    Add project files.

---
+++ Controllers/DrinksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filipan_Dănuț_Proiect.Controllers
+{
+    public class DrinkSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
